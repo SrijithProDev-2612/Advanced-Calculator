95a371b7c2f6ad15011bd2d0a9ce91c26275d5cf
Srijith A <93836444+SrijithProDev-2612@users.noreply.github.com>
2022-02-23 18:21:11 +0530

    Create Kelvin to Celsius Converter.py

---
+++ units-converter/Kelvin to Celsius Converter.py
@@ -0,0 +1,6 @@
+# Program to convert Kelvins (K) to Celsius (C)
+# Authored by Srijith A
+
+c = float(input("Enter temperature in C-Scale: "))
+k = c + 273.15
+print("The tempeature in K-Scale:",round(k,2))
